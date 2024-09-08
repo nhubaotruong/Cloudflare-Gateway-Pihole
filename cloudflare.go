efcdc625fbdfc0b7de326c4ece6f4baa91cc4b44
nhubao <baoprokute1234@gmail.com>
2024-09-08 16:02:57 +0700

    Use library to simplify code

--- cloudflare.go
+++ cloudflare.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
@@ -11,41 +12,31 @@ import (
 )
 
 var (
-	cf_identifier string
+	account_id    string
 	http_client   *req.Client
+	cf_client     *cloudflare.API
+	cf_identifier *cloudflare.ResourceContainer
+	ctx           context.Context
 )
 
 func init() {
 	cf_api_token, has_cf_api_token := os.LookupEnv("CF_API_TOKEN")
-	cf_identifier_1, has_cf_identifier := os.LookupEnv("CF_IDENTIFIER")
+	account_id_t, has_cf_identifier := os.LookupEnv("CF_IDENTIFIER")
 	if !(has_cf_api_token && has_cf_identifier) {
 		log.Fatalln("Please set CF_API_TOKEN and CF_IDENTIFIER")
 	}
-	cf_identifier = cf_identifier_1
-	http_client = req.NewClient()
-	http_client.SetCommonBearerAuthToken(cf_api_token)
-	http_client.SetBaseURL("https://api.cloudflare.com")
-	http_client.SetCommonContentType("application/json")
-	http_client.SetCommonHeader("Accept", "application/json")
+	account_id = account_id_t
+	cf_identifier = cloudflare.AccountIdentifier(account_id)
+	cf_client, _ = cloudflare.NewWithAPIToken(cf_api_token)
+	ctx = context.Background()
 }
 
 func get_cf_lists(name_prefix string) []cloudflare.TeamsList {
-	resp := http_client.Get(fmt.Sprintf("/client/v4/accounts/%s/gateway/lists", cf_identifier)).Do()
-	if resp.Err != nil {
-		log.Fatalln("Error response get_cf_lists", resp.Err.Error(), "body", resp.String())
-	} else if resp.StatusCode != 200 {
-		log.Fatalln("Error response get_cf_lists", resp.StatusCode, "body", resp.String())
-	}
-	// Read body as marshalled json
-	var result cloudflare.TeamsListListResponse
-	err := resp.UnmarshalJson(&result)
+	lists, _, err := cf_client.ListTeamsLists(ctx, cf_identifier, cloudflare.ListTeamListsParams{})
 	if err != nil {
-		log.Fatalln("Error unmarshalling json", err.Error())
+		log.Println("Error response get_cf_lists", err.Error())
 		return []cloudflare.TeamsList{}
 	}
-	// Get lists
-	lists := result.Result
-	// Filter lists
 	filtered_lists := []cloudflare.TeamsList{}
 	for _, v := range lists {
 		if strings.HasPrefix(v.Name, name_prefix) {
@@ -60,51 +51,33 @@ func create_cf_list(name string, domains []string) cloudflare.TeamsList {
 	for _, d := range domains {
 		items = append(items, cloudflare.TeamsListItem{Value: d})
 	}
-	req_json := cloudflare.TeamsList{
+	req_json := cloudflare.CreateTeamsListParams{
 		Name:        name,
 		Description: "Created by script.",
 		Type:        "DOMAIN",
 		Items:       items,
 	}
-	request := http_client.Post(fmt.Sprintf("/client/v4/accounts/%s/gateway/lists", cf_identifier))
-	request.SetBodyJsonMarshal(req_json)
-	resp := request.Do()
-	if resp.Err != nil {
-		log.Fatalln("Error response create_cf_list", resp.Err.Error(), "body", resp.String())
-	} else if resp.StatusCode != 200 {
-		log.Fatalln("Error response create_cf_list", resp.StatusCode, "body", resp.String())
-	}
-
-	var result cloudflare.TeamsListDetailResponse
-	err := resp.UnmarshalJson(&result)
+	cf_list, err := cf_client.CreateTeamsList(ctx, cf_identifier, req_json)
 	if err != nil {
-		log.Fatalln("Error unmarshalling json", err.Error())
+		log.Println("Error response create_cf_list", err.Error())
 		return cloudflare.TeamsList{}
 	}
-	return result.Result
+	return cf_list
 }
 
 func delete_cf_list(list_id string) {
-	resp := http_client.Delete(fmt.Sprintf("/client/v4/accounts/%s/gateway/lists/%s", cf_identifier, list_id)).Do()
-	if resp.Err != nil {
-		log.Fatalln("Error response delete_cf_list", resp.Err.Error(), "body", resp.String())
-	} else if resp.StatusCode != 200 {
-		log.Fatalln("Error response delete_cf_list", resp.StatusCode, "body", resp.String())
+	err := cf_client.DeleteTeamsList(ctx, cf_identifier, list_id)
+	if err != nil {
+		log.Println("Error response delete_cf_list", err.Error())
 	}
 }
 
 func get_gateway_policies(name_prefix string) []cloudflare.TeamsRule {
-	resp := http_client.Get(fmt.Sprintf("/client/v4/accounts/%s/gateway/rules", cf_identifier)).Do()
-	if resp.Err != nil {
-		log.Fatalln("Error response get_gateway_policies", resp.Err.Error(), "body", resp.String())
-	}
-	var result cloudflare.TeamsRulesResponse
-	err := resp.UnmarshalJson(&result)
+	policies, err := cf_client.TeamsRules(ctx, account_id)
 	if err != nil {
-		log.Fatalln("Error unmarshalling json", err.Error())
+		log.Println("Error response get_gateway_policies", err.Error())
 		return []cloudflare.TeamsRule{}
 	}
-	policies := result.Result
 	filtered_policies := []cloudflare.TeamsRule{}
 	for _, v := range policies {
 		if strings.HasPrefix(v.Name, name_prefix) {
@@ -112,7 +85,6 @@ func get_gateway_policies(name_prefix string) []cloudflare.TeamsRule {
 		}
 	}
 	return filtered_policies
-
 }
 
 func create_gateway_policy(name string, list_ids []string) {
@@ -131,15 +103,10 @@ func create_gateway_policy(name string, list_ids []string) {
 			BlockPageEnabled: false,
 		},
 	}
-	req := http_client.Post(fmt.Sprintf("/client/v4/accounts/%s/gateway/rules", cf_identifier))
-	req.SetBodyJsonMarshal(teams_rule)
-	resp := req.Do()
-	if resp.Err != nil {
-		log.Fatalln("Error response create_gateway_policy", resp.Err.Error(), "body", resp.String())
-	} else if resp.StatusCode != 200 {
-		log.Fatalln("Error response create_gateway_policy", resp.StatusCode, "body", resp.String())
+	_, err := cf_client.TeamsCreateRule(ctx, account_id, teams_rule)
+	if err != nil {
+		log.Println("Error response create_gateway_policy", err.Error())
 	}
-	// cf_client.TeamsCreateRule(ctx, cf_identifier, teams_rule)
 }
 
 func delete_gateway_policy(policy_name_prefix string) int {
@@ -148,12 +115,9 @@ func delete_gateway_policy(policy_name_prefix string) int {
 		return 0
 	}
 	policy_id := policies[0].ID
-	resp := http_client.Delete(fmt.Sprintf("/client/v4/accounts/%s/gateway/rules/%s", cf_identifier, policy_id)).Do()
-	if resp.Err != nil {
-		log.Fatalln("Error response delete_gateway_policy", resp.Err.Error(), "body", resp.String())
-		return 0
-	} else if resp.StatusCode != 200 {
-		log.Fatalln("Error response delete_gateway_policy", resp.StatusCode, "body", resp.String())
+	err := cf_client.TeamsDeleteRule(ctx, account_id, policy_id)
+	if err != nil {
+		log.Println("Error response delete_gateway_policy", err.Error())
 		return 0
 	}
 	return 1
--- main.go
+++ main.go
@@ -12,6 +12,21 @@ import (
 var api_sleep_time = 4 * time.Second
 
 func main() {
+	for {
+		if exec() == 0 {
+			break
+		}
+		time.Sleep(api_sleep_time)
+	}
+}
+
+func exec() int {
+	defer func() int {
+		if r := recover(); r != nil {
+			return 1
+		}
+		return 0
+	}()
 	white_list := read_domain_urls("whitelists.txt")
 	white_list_set := convert_to_domain_set(white_list, true, nil)
 	black_list := read_domain_urls("lists.txt")
@@ -29,7 +44,7 @@ func main() {
 
 	// Write to file
 	// os.WriteFile("block_list.txt", []byte(strings.Join(black_list_list, "\n")), 0644)
-	// return
+	// return 0
 
 	// Get cf lists
 	prefix := "[AdBlock-DNS Block List]"
@@ -42,7 +57,7 @@ func main() {
 	}
 	if len(black_list_list) == sum {
 		log.Println("Lists are the same size, skipping")
-		return
+		return 0
 	}
 
 	policy_prefix := fmt.Sprintf("%s Block Ads", prefix)
@@ -53,7 +68,7 @@ func main() {
 	for _, v := range cf_lists {
 		log.Println("Deleting list", v.Name, "- ID:", v.ID)
 		delete_cf_list(v.ID)
-		time.Sleep(api_sleep_time)
+		// time.Sleep(api_sleep_time)
 	}
 
 	// Create cf lists by 1000 chunks
@@ -70,7 +85,7 @@ func main() {
 		log.Println("Creating list", name)
 		cf_list := create_cf_list(name, black_list_list[i:end])
 		new_cf_lists = append(new_cf_lists, cf_list)
-		time.Sleep(api_sleep_time)
+		// time.Sleep(api_sleep_time)
 	}
 
 	// Create cf policies
@@ -78,9 +93,14 @@ func main() {
 	for _, v := range new_cf_lists {
 		new_cf_lists_ids = append(new_cf_lists_ids, v.ID)
 	}
-	// expected_cf_list_count := chunk_counter
-	// actual_cf_list_count := len(new_cf_lists_ids)
+	expected_cf_list_count := chunk_counter
+	actual_cf_list_count := len(new_cf_lists_ids)
+	if expected_cf_list_count != actual_cf_list_count {
+		log.Println("Expected", expected_cf_list_count, "lists, but got", actual_cf_list_count)
+		return 1
+	}
 	log.Println("Creating firewall policy")
 	create_gateway_policy(policy_prefix, new_cf_lists_ids)
 	log.Println("Done!")
+	return 0
 }
